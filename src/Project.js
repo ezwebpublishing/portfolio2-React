3d6b4a04d89c7d50b3262fad7416b1c624276dbf
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-07-22 18:24:25 +0900

    a태그 수정

--- src/Project.js
+++ src/Project.js
@@ -55,7 +55,7 @@ function Project() {
                 <h2 className="tit-h1">르세라핌 리뉴얼 페이지</h2>
                 <p>개인 프로젝트(기획 ~ 구현)</p>
                 <p>바닐라 자바스크립트로 페이지 구현하였고, 구글 달력 API를 연동하였습니다.</p>
-                <a href="https://lesserafimpage.netlify.app/" className="button" data-btn="web site 바로가기"></a>
+                <a href="https://lesserafimpage.netlify.app/" className="button" data-btn="web site 바로가기" target="_blank" rel="noreferrer" aria-label="More lesserafim"></a>
               </figcaption>
             </figure>
           </li>
@@ -66,7 +66,7 @@ function Project() {
                 <h2 className="tit-h1">삼성SDI 리뉴얼 페이지</h2>
                 <p>팀 프로젝트 : 메인 섹션(전체), 서브 페이지(비지니스), 팝업</p>
                 <p>반응형 페이지로 제이쿼리로 구현하였습니다.</p>
-                <a href="https://dubumom.github.io/SamsungSDI/" className="button" data-btn="site 바로가기"></a>
+                <a href="https://dubumom.github.io/SamsungSDI/" className="button" data-btn="site 바로가기" target="_blank" rel="noreferrer" aria-label="More samsungSDI"></a>
               </figcaption>
             </figure>
           </li>
@@ -77,7 +77,7 @@ function Project() {
                 <h2 className="tit-h1">ccc coding 관리자 페이지(LMS site)</h2>
                 <p>팀 프로젝트 : 대쉬보드 메인(전체), 강의 관련 전체, 로그인 관련 전체, 팝업</p>
                 <p>php, mysql, bootstrap으로 구현했습니다.</p>
-                <a href="http://ccccoding.dothome.co.kr/ccccoding/admin/login.php" className="button" data-btn="site 바로가기"></a>
+                <a href="http://ccccoding.dothome.co.kr/ccccoding/admin/login.php" className="button" data-btn="site 바로가기" target="_blank" rel="noreferrer" aria-label="ccc coding"></a>
               </figcaption>
             </figure>
           </li>
@@ -88,7 +88,7 @@ function Project() {
                 <h2 className="tit-h1">ccc coding 사용자 페이지(LMS site)</h2>
                 <p>팀 프로젝트 : 메인 섹션(베너슬라이드, 강의 바로가기, 신규강의, 베스트 강의), 카테고리 관련 전체, 로그인 관련 전체, 팝업</p>
                 <p>php, mysql, bootstrap으로 구현했습니다.</p>
-                <a href="http://ccccoding.dothome.co.kr/ccccoding/" className="button" data-btn="site 바로가기"></a>
+                <a href="http://ccccoding.dothome.co.kr/ccccoding/" className="button" data-btn="site 바로가기" target="_blank" rel="noreferrer" aria-label="ccc coding user"></a>
               </figcaption>
             </figure>
           </li>
@@ -100,7 +100,7 @@ function Project() {
                 <p>개인 프로젝트(기획 ~ 구현)</p>
                 <p>적응형 페이지로 제이쿼리로 구현했습니다.</p>
                 <a href="https://dubumom.github.io/Are-you-Hungry/" className="button mr" data-btn="web site 바로가기"></a>
-                <a href="https://dubumom.github.io/Are-you-Hungry/m_index.html" className="button" data-btn="mobile site 바로가기"></a>
+                <a href="https://dubumom.github.io/Are-you-Hungry/m_index.html" className="button" data-btn="mobile site 바로가기" target="_blank" rel="noreferrer" aria-label="are you hungry"></a>
               </figcaption>
             </figure>
           </li>
@@ -111,7 +111,7 @@ function Project() {
                 <h2 className="tit-h1">판다월드 제작 페이지</h2>
                 <p>개인 프로젝트(기획 ~ 구현)</p>
                 <p>퍼블리싱을 배우며 처음 제작한 페이지 입니다.</p>
-                <a href="https://dubumom.github.io/panda_world/" className="button" data-btn="site 바로가기"></a>
+                <a href="https://dubumom.github.io/panda_world/" className="button" data-btn="site 바로가기" target="_blank" rel="noreferrer" aria-label="panda world"></a>
               </figcaption>
             </figure>
           </li>
@@ -122,7 +122,7 @@ function Project() {
                 <h2 className="tit-h1">오키친 리뉴얼 페이지</h2>
                 <p>개인 프로젝트(기획 ~ 구현)</p>
                 <p>반응형 페이지로 제이쿼리로 구현했습니다.</p>
-                <a href="https://dubumom.github.io/Okichin/" className="button" data-btn="site 바로가기"></a>
+                <a href="https://dubumom.github.io/Okichin/" className="button" data-btn="site 바로가기" target="_blank" rel="noreferrer" aria-label="okichin"></a>
               </figcaption>
             </figure>
           </li>          
--- src/Work.js
+++ src/Work.js
@@ -21,7 +21,7 @@ function Work() {
                 <p>원페이지 레이아웃으로 디자인 하였으며, 기존 소스뮤직 사이트를 참고하여 웹페이지의 접근성을 높히고 심미성 증진을 목표로 기획 하였습니다.</p>
                 <p>제작 기한 : 약 3주</p>
                 <p>피그마, 포토샵, 구글 캘린더 </p>
-                <a href="https://www.figma.com/proto/2gckeiReA5euFJkdkTq4Kl/1%EC%B0%A8-%EA%B3%BC%EC%A0%9C-%ED%8E%98%EC%9D%B4%EC%A7%80-%EB%A6%AC%EB%89%B4%EC%96%BC---%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C?page-id=30%3A117&node-id=30-118&viewport=441%2C424%2C0.02&t=RdYxVRgXAPf7NpXe-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/2gckeiReA5euFJkdkTq4Kl/1%EC%B0%A8-%EA%B3%BC%EC%A0%9C-%ED%8E%98%EC%9D%B4%EC%A7%80-%EB%A6%AC%EB%89%B4%EC%96%BC---%EB%A5%B4%EC%84%B8%EB%9D%BC%ED%95%8C?page-id=30%3A117&node-id=30-118&viewport=441%2C424%2C0.02&t=RdYxVRgXAPf7NpXe-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank" rel="noreferrer" aria-label="르세라핌 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className="">
@@ -34,7 +34,7 @@ function Work() {
                 <p>php와 mysql을 활용하여 lms사이트를 제작하였으며 관리자가 데이터를 기반으로 사이트를 조작할 수 있는 페이지입니다. </p>
                 <p>제작 기한 : 약 4주</p>
                 <p>피그마, 포토샵, 엑셀, 슬렉</p>
-                <a href="https://www.figma.com/proto/yIwo11tJX1epVXVyCZwkfm/%EC%8B%9C%ED%81%AC%EB%A6%BF-%EC%A5%AC%EC%A5%AC-2?page-id=641%3A5456&node-id=670-4919&viewport=814%2C1101%2C0.03&t=0YWGqwIaFGc5jM8H-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/yIwo11tJX1epVXVyCZwkfm/%EC%8B%9C%ED%81%AC%EB%A6%BF-%EC%A5%AC%EC%A5%AC-2?page-id=641%3A5456&node-id=670-4919&viewport=814%2C1101%2C0.03&t=0YWGqwIaFGc5jM8H-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank" rel="noreferrer" aria-label="ccccoding 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className=""> 
@@ -47,7 +47,7 @@ function Work() {
                 <p>php와 mysql을 활용하여 lms사이트를 제작하였으며 깔끔한 구성, 사용자 친화적인 UI/UX를 염두하여 만들었습니다. 강의 배너, 이벤트 이미지들은 모두 자체 제작하였습니다.</p>
                 <p>제작 기한 : 약 4주</p>
                 <p>피그마, 포토샵, 엑셀, 슬렉</p>
-                <a href="https://www.figma.com/proto/yIwo11tJX1epVXVyCZwkfm/%EC%8B%9C%ED%81%AC%EB%A6%BF-%EC%A5%AC%EC%A5%AC-2?page-id=641%3A5456&node-id=670-4919&viewport=814%2C1101%2C0.03&t=0YWGqwIaFGc5jM8H-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/yIwo11tJX1epVXVyCZwkfm/%EC%8B%9C%ED%81%AC%EB%A6%BF-%EC%A5%AC%EC%A5%AC-2?page-id=641%3A5456&node-id=670-4919&viewport=814%2C1101%2C0.03&t=0YWGqwIaFGc5jM8H-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank" rel="noreferrer" aria-label="ccc coding 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className="">
@@ -60,7 +60,7 @@ function Work() {
                 <p>적응형 사이트로 모바일과 웹 페이지를 구현하였습니다. 전통시장의 부흥과 활성화를 위해 해당 주제를 선정하였으며, 기획부터 구현까지 100% 제작 페이지 입니다.</p>
                 <p>제작 기한 : 약 4주</p>
                 <p>피그마, 포토샵, 일러스트</p>
-                <a href="https://www.figma.com/proto/7XQQkHH0qMk1BTprlcXamZ/%EC%8B%9C%EC%9E%A5%ED%95%98%EC%8B%9C%EC%A3%A0-%EC%A0%9C%EC%B6%9C%ED%8E%98%EC%9D%B4%EC%A7%80?page-id=0%3A1&node-id=669-1417&viewport=589%2C338%2C0.02&t=WiBWTMVlaBGegie1-1&scaling=scale-down-width&content-scaling=fixed" className="tit-h4 shortcut">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/7XQQkHH0qMk1BTprlcXamZ/%EC%8B%9C%EC%9E%A5%ED%95%98%EC%8B%9C%EC%A3%A0-%EC%A0%9C%EC%B6%9C%ED%8E%98%EC%9D%B4%EC%A7%80?page-id=0%3A1&node-id=669-1417&viewport=589%2C338%2C0.02&t=WiBWTMVlaBGegie1-1&scaling=scale-down-width&content-scaling=fixed" className="tit-h4 shortcut" rel="noreferrer" aria-label="시장하시죠 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className="">
@@ -73,7 +73,7 @@ function Work() {
                 <p>반응형 사이트로 기존에 페이지를 보다 동적으로 새롭게 리뉴얼하였습니다. 기업 페이지에 맞게 화려한 인터페이스보다는 무게감 있으면서도 디테일한 에니메이션 효과에 중점을 두었습니다.</p>
                 <p>제작 기한 : 약 4주</p>
                 <p>피그마, 포토샵</p>
-                <a href="https://www.figma.com/proto/TSC8SxmxGCVYAtVqdF8aSC/%EC%82%BC%EC%84%B1SDI-%EB%A6%AC%EB%89%B4%EC%96%BC?page-id=0%3A1&node-id=414-500&viewport=-287%2C855%2C0.02&t=Q0HSJCA37u52WOjT-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/TSC8SxmxGCVYAtVqdF8aSC/%EC%82%BC%EC%84%B1SDI-%EB%A6%AC%EB%89%B4%EC%96%BC?page-id=0%3A1&node-id=414-500&viewport=-287%2C855%2C0.02&t=Q0HSJCA37u52WOjT-1&scaling=min-zoom&content-scaling=fixed" className="tit-h4 shortcut" target="_blank" rel="noreferrer" aria-label="삼성SDI 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className="">
@@ -86,7 +86,7 @@ function Work() {
                 <p>이벤트 페이지로 적합한 원페이지 레이아웃으로 구성하였으며, 푸바오를 좋아하는 마음을 담아 제작하였습니다. 푸바오를 닮은 귀엽고 사랑스러운 느낌을 주려고 노력하였습니다.</p>
                 <p>제작 기한 : 약 2주</p>
                 <p>피그마, 포토샵, 일러스트</p>
-                <a href="https://www.figma.com/proto/CjyRdoOzDLwRgdgfGfX4BU/%EA%B5%AC%EB%91%90%EB%B0%9C%ED%91%9C-%ED%8F%89%EA%B0%80-%EC%A0%9C%EC%B6%9C%ED%8E%98%EC%9D%B4%EC%A7%80?page-id=25%3A111&node-id=25-112&viewport=442%2C333%2C0.09&t=S3OeY78za0Jr26nu-1&scaling=contain&content-scaling=fixed" className="tit-h4 shortcut" target="_blank">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/CjyRdoOzDLwRgdgfGfX4BU/%EA%B5%AC%EB%91%90%EB%B0%9C%ED%91%9C-%ED%8F%89%EA%B0%80-%EC%A0%9C%EC%B6%9C%ED%8E%98%EC%9D%B4%EC%A7%80?page-id=25%3A111&node-id=25-112&viewport=442%2C333%2C0.09&t=S3OeY78za0Jr26nu-1&scaling=contain&content-scaling=fixed" className="tit-h4 shortcut" target="_blank" rel="noreferrer" aria-label="푸바오 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className="">
@@ -99,7 +99,7 @@ function Work() {
                 <p>기존 오키친 사이트가 반응형으로 리뉴얼 되기 전 먼저 반응형으로 구성해본 사이트 입니다. 레시피를 필요로 하는 주부, 자취생을 타겟으로 키워드에 맞는 정보를 제공하고 명료한 정보전달을 목표로 제작하였습니다.</p>
                 <p>제작 기한 : 약 5주</p>
                 <p>피그마, 포토샵, 일러스트</p>
-                <a href="https://www.figma.com/proto/gIeAZXRXDeyb1GKToWaWZk/'%EC%98%A4'%ED%82%A4%EC%B9%9C?page-id=0%3A1&node-id=1-290&viewport=442%2C373%2C0.06&t=B2LgsDQS2Hipz8KF-1&scaling=contain&content-scaling=fixed" className="tit-h4 shortcut" target="_blank">바로가기 &#187;</a>
+                <a href="https://www.figma.com/proto/gIeAZXRXDeyb1GKToWaWZk/'%EC%98%A4'%ED%82%A4%EC%B9%9C?page-id=0%3A1&node-id=1-290&viewport=442%2C373%2C0.06&t=B2LgsDQS2Hipz8KF-1&scaling=contain&content-scaling=fixed" className="tit-h4 shortcut" target="_blank" rel="noreferrer" aria-label="오키친 기획">바로가기 &#187;</a>
               </div>
             </li>
             <li className="">
@@ -112,7 +112,7 @@ function Work() {
                 <p>전국의 박물관 정보를 제공하고 예약, 예매까지 연동할 수 있는 앱 사이트를 기획 및 디자인 하였습니다.</p>
                 <p>제작 기한 : 약 6주</p>
                 <p>피그마, 포토샵, 일러스트</p>
-                <a href="https://www.figma.com/proto/6p6qejjFNBqRfho40zFvmk/%ED%98%9C%EB%A6%AC%ED%8B%B0-%EB%AE%A4%EC%A7%80%EC%97%84%EA%B3%A0(%EB%B0%95%EB%AC%BC%EA%B4%80-APP)?page-id=34%3A504&node-id=325-5296&viewport=621%2C496%2C0.03&t=yLg4LdNkoRUYqVb4-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=222%3A5457&show-proto-sidebar=1"></a>
+                <a href="https://www.figma.com/proto/6p6qejjFNBqRfho40zFvmk/%ED%98%9C%EB%A6%AC%ED%8B%B0-%EB%AE%A4%EC%A7%80%EC%97%84%EA%B3%A0(%EB%B0%95%EB%AC%BC%EA%B4%80-APP)?page-id=34%3A504&node-id=325-5296&viewport=621%2C496%2C0.03&t=yLg4LdNkoRUYqVb4-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=222%3A5457&show-proto-sidebar=1" target="_blank" rel="noreferrer" aria-label="박물관고 기획"></a>
               </div>
             </li>
           </ul>
